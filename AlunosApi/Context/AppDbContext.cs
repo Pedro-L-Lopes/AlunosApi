8f7a9cc5a7df4bdebb5cea7a57e7a94f3616df0a
Pedro Lucas <pedrolucas122737@gmail.com>
2024-01-13 12:33:54 -0300

    First commit

---
+++ AlunosApi/Context/AppDbContext.cs
@@ -0,0 +1,37 @@
+﻿using AlunosApi.Models;
+using Microsoft.EntityFrameworkCore;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace AlunosApi.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        // Mapeando tabelas no banco de dados
+        public DbSet<Aluno> Alunos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Aluno>().HasData(
+                new Aluno
+                {
+                    Id = 1,
+                    Nome = "Zezin do grau",
+                    Email = "zezin@email.com",
+                    Idade = 20
+                },
+                 new Aluno
+                 {
+                Id = 2,
+                    Nome = "Wilson",
+                    Email = "Wilson@email.com",
+                    Idade = 35
+                }
+            );
+        }
+    }
+}
---
+++ AlunosApi/Migrations/20240112154245_PopulaTabela.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AlunosApi.Migrations
+{
+    public partial class PopulaTabela : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Alunos",
+                columns: new[] { "Id", "Email", "Idade", "Nome" },
+                values: new object[] { 1, "zezin@email.com", 20, "Zezin do grau" });
+
+            migrationBuilder.InsertData(
+                table: "Alunos",
+                columns: new[] { "Id", "Email", "Idade", "Nome" },
+                values: new object[] { 2, "Wilson@email.com", 35, "Wilson" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Alunos",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Alunos",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
