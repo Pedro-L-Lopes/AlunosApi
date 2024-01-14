0da6a43023d39682713cae7dee139e09d8df05d8
Pedro Lucas <pedrolucas122737@gmail.com>
2024-01-14 14:32:05 -0300

    Habilitando cors

--- AlunosApi/Program.cs
+++ AlunosApi/Program.cs
@@ -31,6 +31,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseMiddleware(typeof(GlobalErrorHandlingMiddleware));
 
+app.UseCors(opt => opt.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
 app.UseHttpsRedirection();
 
 app.UseRouting();
