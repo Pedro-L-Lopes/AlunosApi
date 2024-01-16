627f2c4f16d931aed190e098f56cd0422b9a5eba
Pedro Lucas <pedrolucas122737@gmail.com>
2024-01-16 12:14:55 -0300

    AccountController, HttpPost CreateUser

---
+++ AlunosApi/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using AlunosApi.Services;
+using AlunosApi.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlunosApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IAuthenticate _authentication;
+
+        public AccountController(IConfiguration configuration, IAuthenticate authentication)
+        {
+            _configuration = configuration ?? 
+                throw new ArgumentNullException(nameof(configuration));
+
+            _authentication = authentication ??
+                throw new ArgumentNullException(nameof(authentication));
+        }
+
+        [HttpPost("CreateUser")]
+        public async Task<ActionResult<UserToken>> CreateUser([FromBody] RegisterModel model)
+        {
+            if(model.Password != model.ConfirmPassword)
+            {
+                ModelState.AddModelError("ConfirmePassword", "As senhas não conferem");
+                return BadRequest(ModelState);
+            }
+
+            var result = await _authentication.RegisterUser(model.Email, model.Password);
+
+            if (result)
+            {
+                return Ok($"Usuário {model.Email} criado com sucesso");
+            }
+            else
+            {
+                ModelState.AddModelError("CreateUser", "Regsitro inválido.");
+                return BadRequest(ModelState);
+            }
+        }
+    }
+}
