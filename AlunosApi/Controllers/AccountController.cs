f1ebdc690d0a36d0ef43b020f2aee70b38542307
Pedro Lucas <pedrolucas122737@gmail.com>
2024-01-17 10:52:21 -0300

    Finalizando a implementação do JWT

--- AlunosApi/Controllers/AccountController.cs
+++ AlunosApi/Controllers/AccountController.cs
@@ -1,10 +1,14 @@
 ﻿using AlunosApi.Services;
 using AlunosApi.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace AlunosApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]")] 
     [ApiController]
     public class AccountController : ControllerBase
     {
@@ -13,7 +17,7 @@ namespace AlunosApi.Controllers
 
         public AccountController(IConfiguration configuration, IAuthenticate authentication)
         {
-            _configuration = configuration ?? 
+            _configuration = configuration ??
                 throw new ArgumentNullException(nameof(configuration));
 
             _authentication = authentication ??
@@ -23,7 +27,7 @@ namespace AlunosApi.Controllers
         [HttpPost("CreateUser")]
         public async Task<ActionResult<UserToken>> CreateUser([FromBody] RegisterModel model)
         {
-            if(model.Password != model.ConfirmPassword)
+            if (model.Password != model.ConfirmPassword)
             {
                 ModelState.AddModelError("ConfirmePassword", "As senhas não conferem");
                 return BadRequest(ModelState);
@@ -40,6 +44,50 @@ namespace AlunosApi.Controllers
                 ModelState.AddModelError("CreateUser", "Regsitro inválido.");
                 return BadRequest(ModelState);
             }
+        }
+
+        [HttpPost("LoginUser")]
+        public async Task<ActionResult<UserToken>> Login([FromBody] LoginModel userInfo)
+        {
+            var result = await _authentication.Authenticate(userInfo.Email, userInfo.Password);
+
+            if (result)
+            {
+                return GenerateToken(userInfo);
+            }
+            else
+            {
+                ModelState.AddModelError("LoginUser", "Login inválido.");
+                return BadRequest(ModelState);
+            }
+        }
+
+        private ActionResult<UserToken> GenerateToken(LoginModel userInfo)
+        {
+            var claims = new[]
+            {
+                new Claim("email", userInfo.Email),
+                new Claim("meu token", "token do pedro"),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:key"]));
+
+            var creeds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var expiration = DateTime.UtcNow.AddMinutes(20);
+
+            JwtSecurityToken token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: expiration,
+                signingCredentials: creeds);
+
+            return new UserToken()
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                Expiration = expiration,
+            };
         }
     }
 }
--- AlunosApi/Controllers/AlunosController.cs
+++ AlunosApi/Controllers/AlunosController.cs
@@ -1,5 +1,7 @@
 ﻿using AlunosApi.Models;
 using AlunosApi.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,6 +9,7 @@ namespace AlunosApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class AlunosController : ControllerBase
     {
         private IAlunoService _alunoService;
